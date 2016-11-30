c81633894aba8bada89d6de8a6bbd19d08150aa8
Chris McDonald <cjmcdona@butler.edu>
2016-11-30 14:40:37 -0500

    Cleaned up Calendar Controller

--- src/main/angularjs/com.ronaldmcdonaldhouse.volunteerservice/static/js/controller/CalendarController.js
+++ src/main/angularjs/com.ronaldmcdonaldhouse.volunteerservice/static/js/controller/CalendarController.js
@@ -18,12 +18,12 @@ volunteerService.controller('CalendarController',
         };
         /* event source that contains custom events on the scope */
         $scope.events = [
-            {title: 'All Day Event',start: new Date(y, m, 1)},
-            {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-            {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-            {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-            {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-            {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+            // {title: 'All Day Event',start: new Date(y, m, 1)},
+            // {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+            // {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+            // {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+            // {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+            // {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
         ];
         /* event source that calls a function on every view switch */
         $scope.eventsF = function (start, end, timezone, callback) {
@@ -34,15 +34,15 @@ volunteerService.controller('CalendarController',
             callback(events);
         };
 
-        $scope.calEventsExt = {
-            color: '#f00',
-            textColor: 'yellow',
-            events: [
-                {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-                {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-                {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-            ]
-        };
+        // $scope.calEventsExt = {
+        //     color: '#f00',
+        //     textColor: 'yellow',
+        //     events: [
+        //         {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+        //         {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+        //         {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+        //     ]
+        // };
         $scope.clickOnDate = function(date, jsEvent, view)
         {
             $scope.day = date.format('D');
